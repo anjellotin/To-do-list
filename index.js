775efaf601486a0e9b521617d8dc860784a00811
Angelo Andrade <anjelloandrade@gmail.com.github.com>
2023-02-12 02:49:17 -0800

    working on JS

---
+++ index.js
@@ -0,0 +1,13 @@
+let addSection = document.querySelector(".add-container");
+let doneSection = document.querySelector(".done-container");
+let input = document.getElementById("input-sec");
+
+
+
+//use DOM to create cards from input value
+
+//get cards and put in added tools
+
+//create event to drag into done tools
+
+// local storage save
